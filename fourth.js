409cddd4c13458dbc4dce0b61e7b785557595cd4
PRASS-NAA <prassml23@gmail.com>
2025-06-06 15:24:49 +0530

    added fourth.js and also made the bug dissapear so index,js is the stable version

---
+++ fourth.js
@@ -0,0 +1,2 @@
+console.log("keep this idiotic file untracked ! ")
+console.log("use .gitignore or rm ");
--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log("omg its been long since i coded ");
 console.log("but this isnt to revise coding ! this is to practice git and understand stuffs other than push and pull");
 
 console.log("new line ra");
+
+console.log("stable version 1");
