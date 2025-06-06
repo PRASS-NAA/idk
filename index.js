93833f0e9c9b9d8d79cc419d15b754b1a8ac6102
PRASS-NAA <prassml23@gmail.com>
2025-06-06 15:07:07 +0530

    first commit, lets start to understand things

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("omg its been long since i coded ");
+
+console.log("but this isnt to revise coding ! this is to practice git and understand stuffs other than push and pull");
+
+console.log("new line ra");
---
+++ second.js
@@ -0,0 +1,2 @@
+console.log("ill use this file to see how untracked and tracked files are different");
+console.log("stupid");
