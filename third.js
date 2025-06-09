5fa03f2b12875cb7dd9400b1bc40afafdb2045e2
PRASS-NAA <prassml23@gmail.com>
2025-06-09 10:51:26 +0530

    changed from master branch

--- third.js
+++ third.js
@@ -3,4 +3,4 @@ console.log("i will learn commits branching merging using the tree ");
 console.log("i didnt stage this");
 console.log("i fixed this bug");
 console.log("actual bug fix");
-console.log("will this cause mr");
+console.log("will this cause mr, allahuakbar");
