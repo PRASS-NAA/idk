7ec4a89f086332ea0649bc6e077fd941f90a3705
PRASS-NAA <prassml23@gmail.com>
2025-06-09 09:49:27 +0530

    actuallu i fixed bug on third.js

--- third.js
+++ third.js
@@ -2,3 +2,4 @@ console.log("this is third file ");
 console.log("i will learn commits branching merging using the tree ");
 console.log("i didnt stage this");
 console.log("i fixed this bug");
+console.log("actual bug fix");
